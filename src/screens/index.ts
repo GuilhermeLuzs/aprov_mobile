2950d141c41628adf4b74222e901070718dd088a
GuilhermeLuzs <sguilhermeluz@gmail.com>
2026-09-17 18:57:32 -0300

    feat: adiciona tela de permissão de notificações

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as InterestsScreen } from './Interests/InterestsScreen';
 export { default as CompaniesScreen } from './Companies/CompaniesScreen';
 export { default as GoalScreen } from './Goal/GoalScreen';
+export { default as NotificationsPermissionScreen } from './NotificationsPermission/NotificationsPermissionScreen';
