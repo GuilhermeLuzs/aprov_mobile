3b462c5c87b37791e3f6a39562900b3a92ccb186
GuilhermeLuzs <sguilhermeluz@gmail.com>
2026-09-17 18:58:54 -0300

    feat: adiciona tela inicial com carrosséis de produtos

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export { default as InterestsScreen } from './Interests/InterestsScreen';
 export { default as CompaniesScreen } from './Companies/CompaniesScreen';
 export { default as GoalScreen } from './Goal/GoalScreen';
 export { default as NotificationsPermissionScreen } from './NotificationsPermission/NotificationsPermissionScreen';
+export { default as HomeScreen } from './Home/HomeScreen';
